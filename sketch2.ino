f5cbb11bc1c36e3a58e9985a30db51b709b23378
Eduard_Churikov <edik_gres@icloud.com>
2022-05-23 04:56:28 +0300

    fix

--- sketch2.ino
+++ sketch2.ino
@@ -1,5 +1,5 @@
 #include "GyverRelay.h"
-
+#include <LiquidCrystal.h>
 //---------------------Thermistor---------------------
 //pin for Thermistor
 #define THERMISTORPIN A0
@@ -19,42 +19,73 @@
 //---------------------Relay---------------------
 GyverRelay regulator(REVERSE);
 
+
+//---------------------RUCHKA---------------------
+#define RUCHKA A5
+//---------------------RUCHKA---------------------
+//---------------------DISPLAY---------------------
+constexpr uint8_t PIN_RS = 12;
+constexpr uint8_t PIN_EN = 11;
+constexpr uint8_t PIN_DB4 = 5;
+constexpr uint8_t PIN_DB5 = 4;
+constexpr uint8_t PIN_DB6 = 3;
+constexpr uint8_t PIN_DB7 = 2;
+LiquidCrystal lcd(PIN_RS, PIN_EN, PIN_DB4, PIN_DB5, PIN_DB6, PIN_DB7);
+//---------------------DISPLAY---------------------
+
 int samples[NUMSAMPLES];
 float set_temp = 70;
-float hyster = 3;
-
-//---------------------RUCHKA---------------------
-#define RUCHKA A0
-//---------------------RUCHKA---------------------
-
+float hyster = 2;
+int ruchka_resist = 0;
+int counter = 0;
 
 void setup(void) {
 Serial.begin(9600);
 analogReference(EXTERNAL);
 pinMode(RELAY, OUTPUT);
 
-regulator.k = 4;
-regulator.setpoint = set_temp;
+regulator.k = 3;
 regulator.hysteresis = hyster;
+
+lcd.begin(16, 2);
+lcd.setCursor(0, 0);
+
 }
 
 void loop(void) {
-float temp = getTemp();
-Serial.print("Temperature ");
-Serial.print(temp);
-Serial.println(" *C");
-regulator.input = temp;
-static uint32_t tmr;
-  if (millis() - tmr > 500) {
-    tmr = millis();
-    regulator.input = getTemp(); // сообщаем регулятору текущую температуру
-    digitalWrite(RELAY, regulator.getResult());   // отправляем на реле (ОС работает по своему таймеру)
-  }
-//digitalWrite(RELAY, regulator.getResult());
-analogRead(
+  counter++;
+  float temp = getTemp();
+  Serial.print("Temperature ");
+  Serial.print(temp);
+  Serial.println(" *C");
+  regulator.input = temp;
+  static uint32_t tmr;
+    if (millis() - tmr > 500) {
+      tmr = millis();
+      regulator.input = getTemp(); // сообщаем регулятору текущую температуру
+      digitalWrite(RELAY, regulator.getResult());   // отправляем на реле (ОС работает по своему таймеру)
+    }
+  //digitalWrite(RELAY, regulator.getResult());
+  ruchka_resist = analogRead(RUCHKA);
+  set_temp = map(ruchka_resist, 0,1024,0,100);
+  regulator.setpoint = set_temp;
+  Serial.print("Ruchka: ");
+  Serial.println(set_temp);
   
-delay(250);
-
+  lcd.setCursor(0,0);
+  lcd.print("Cur_Temp:");
+  lcd.print(temp);
+  lcd.setCursor(0,1);
+  lcd.print("Set_Temp:");
+  lcd.print(set_temp);
+  delay(250);
+  //lcd.clear();
+  if(counter > 12){
+    lcd.clear();
+    lcd.begin(16, 2);
+    lcd.setCursor(0, 0);
+    counter = 0;
+}
 }
 
 float getTemp() {
